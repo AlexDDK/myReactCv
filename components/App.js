3c5433314eb37eeac9c371314a3cec2ab01e08e3
AlexDDK <dhrsmp@gmail.com>
2022-10-23 21:43:36 +0300

    feat: add foto and grid

--- components/App.js
+++ components/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import myFoto from '../src/img/cdvd.jpg'
 
 export default function App() {
   return (
     <div className='container'>
-        <h1>My CV!!!</h1>
+        {/* <div className='foto-cell'> one </div> */}
+        <div className='foto-cell'>
+          <img src={myFoto} className='myFoto'/>
+        </div>
+        <div> two </div>
+        <div> three </div>
+        <div> four </div>
     </div>
   )
 }
